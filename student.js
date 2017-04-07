66817edca11eca61ec05f36364b4916e3146bfd4
erik morales <emorale112@gmail.com>
2017-04-07 09:01:37 -0500

    did some stuff

---
+++ student.js
@@ -0,0 +1,22 @@
+function Student(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+Student.prototype.getFirstName = function() {
+  return this.firstName;
+}
+
+Student.prototype.getLastName = function() {
+  return this.lastName;
+}
+
+Student.prototype.getFullName = function() {
+  return this.firstName + ' ' + this.lastName;
+}
+
+Student.prototype.getGreeting = function() {
+  return 'Good day mate, my name is ' + this.firstName;
+}
+
+module.exports = Student;
